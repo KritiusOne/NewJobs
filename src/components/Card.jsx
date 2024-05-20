be3f179f71e2431741f7c85ef9b05ae99b815c7f
JairDeLaRosa <128413355+JairDeLaRosa@users.noreply.github.com>
2024-05-19 23:15:21 -0500

    version 1.0

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,16 @@
 import "./card.css"
-export default function Card({title, city, description, ...props}) {
+
+export default function Card({title, city, description, alert}) {
+  
   return (
-    <article {...props} className={`card ${props.className}`}>
+    <article className="card" onClick={alert}>
       <h2 className="card__title"> {title} </h2>
       <strong className="card__city"> {city} </strong>
       <span className="card__description">
         {description}
+      </span>
+      <span className="click">
+        Click para postularte
       </span>
     </article>
   )
